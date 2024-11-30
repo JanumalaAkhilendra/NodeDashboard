dbbb51f06104c4ee274de2cf57741a6ffbe86fc4
Akhilendra <janumalaakhilendra01122000@gmail.com>
2024-11-30 12:16:34 +0530

    a

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,10 @@
 import { DashboardHeader } from "@/components/dashboard/header";
 import { DashboardStats } from "@/components/dashboard/stats";
 import { ActivityChart } from "@/components/dashboard/activity-chart";
-import { TopicsList } from "@/components/dashboard/topics-list";
+import { StrongestTopics } from "@/components/dashboard/topics-list-strongest";
+import { WeakestTopics } from "@/components/dashboard/topics-list-weakest";
 import { Leaderboard } from "@/components/dashboard/leaderboard";
+import data from "@/public/task-data.json";
 export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-6 p-8">
@@ -12,8 +14,8 @@ export default function DashboardPage() {
 
         <DashboardStats />
         <ActivityChart />
-        <TopicsList title="weakest" />
-        <TopicsList title="strongest" />
+        <StrongestTopics/>
+        <WeakestTopics />
         <Leaderboard title="User Leaderboard"  />
         <Leaderboard title="Groups Leaderboard" />
       </div>
